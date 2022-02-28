f2dbeac505acfe96b677ad886f60d7fac3d018cb
rabiaa1001 <49293572+rabiaa1001@users.noreply.github.com>
2022-02-28 08:15:50 -0500

    Update data_aug.py

--- data_aug.py
+++ data_aug.py
@@ -9,11 +9,11 @@ import imageio
 import imgaug.augmenters as iaa
 
 
-ORIGIN_FOLDER_PATH = './images_not_augmented/'
-WRITE_FOLDER_PATH = './images_augmented/'
+ORIGIN_FOLDER_PATH = '/Users/rabia/PycharmProjects/pythonProject/hijjab_vs_non_hijjab_project/python_py_test/Test_images/images_to_augment'
+WRITE_FOLDER_PATH = '/Users/rabia/PycharmProjects/pythonProject/hijjab_vs_non_hijjab_project/python_py_test/Test_images/augmented_images'
 NUM_TO_AUGMENT = 20
 
-def augment_dataset(folder_path:str,write_folder:str,num_images_desired:int):
+def augment_dataset(folder_path:str,write_folder:str,num_files_desired:int) -> str:
     """
     1.Reads through images in the folder_path
     2.Randomly selects files to augment
@@ -36,20 +36,24 @@ def augment_dataset(folder_path:str,write_folder:str,num_images_desired:int):
             os.path.isfile(os.path.join(folder_path, f))])
 
     # Randomly select images to augment based on num_files_desired
-    random_images = np.random.choice(images, size = num_images_desired)
+    random_images = np.random.choice(images, size = num_files_desired)
 
     for each in random_images:
 
         # Read each image
         each_image = imageio.imread(each)
+        
+        # Ensure image is the correct shape
+        if not each_image.shape[2] == 3:
+            each_image = each_image[:,:,:3]
 
         # Object that implements
         # augmentation in a random order
         seq = iaa.Sequential([
             iaa.Affine(rotate=(-25, 25)),
-            iaa.AdditiveGaussianNoise(scale=(0, 30)), # (30, 90)
-            iaa.AddToHueAndSaturation((-10, 10)),# (-20, 20)
-            iaa.Crop(percent=(0, 0.3)) # .2
+            iaa.AdditiveGaussianNoise(scale=(0, 30)),
+            iaa.AddToHueAndSaturation((-10, 10)),
+            iaa.Crop(percent=(0, 0.3))
         ], random_order=True)
 
         # Separate image name
@@ -59,10 +63,10 @@ def augment_dataset(folder_path:str,write_folder:str,num_images_desired:int):
         images_aug = seq(image=each_image)
 
         # Write images to write_folder
-        imageio.imwrite(write_folder + 'augmented.' + an_image,images_aug)
+        imageio.imwrite(write_folder + '_augmented_' + an_image,images_aug)
         print(f"Augmented: {an_image}")
 
-    return print("\nCompleted Augmentation")
+    return f'Completed: {len(os.listdir(write_folder))}'
 
 if __name__ == '__main__':
     augment_dataset(ORIGIN_FOLDER_PATH,WRITE_FOLDER_PATH,NUM_TO_AUGMENT)
